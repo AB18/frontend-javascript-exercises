b2c22fecc5df387e65c4cf0d833a8f3823036591
AB <ashrita.b@gmail.com>
2014-08-09 14:28:18 +0000

    Fixed 04-strings_00-basics

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,22 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+      var body = "Hello " +  firstName + "," + "\n\n" + message + "\n\n" + "Sincerely,\n" + senderName;
+      return body;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  var firstSubString = bigString.substring(startA, endA);
+  var secondSubString = bigString.substring(startB,endB);
+  return firstSubString + secondSubString;
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+    return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+    return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+    return text.substring(text.indexOf(searchString) + searchString.length, text.lastIndexOf(searchString));
 };
