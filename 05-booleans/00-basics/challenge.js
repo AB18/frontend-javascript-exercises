d44206bdcccd66a3061bd3871ba59d7d31d08407
AB <ashrita.b@gmail.com>
2014-08-09 15:21:06 +0000

    Fixed 05-booleans_00-basics

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,37 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+    if (stringOne == stringTwo) 
+      return true;
+    else
+      return false;
 };
 
 module.exports.notEqual = function(one, two) {
-
+    if(one !== two)
+      return true;
+    else 
+      return false;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+    if(lower < middle && middle < upper)
+      return true;
+    else
+      return false;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  if ((10 < number && number < 20) || (42 <= number && number < 75) || (1 <= number && number <= 6) )
+      return true;
+  else if(!(10 < number && number < 20) && !(42 <= number && number < 75) && !(1 <= number && number <= 6)
+    && (number != 10 && number != 20 && number !=75 ) )
+      return true;
+  else
+      return false;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  if(((name == 'NYTimes') || name == 'LATimes') && (price >= 1))
+    return true;
+  else
+    return false;
 };
