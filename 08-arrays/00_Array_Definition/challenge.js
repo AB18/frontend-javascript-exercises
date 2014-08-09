b9017901539e41a7729d152383fcd0036e805751
AB <ashrita.b@gmail.com>
2014-08-09 23:27:20 +0000

    Fixed 00_Array_Definitions

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,12 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(arg1,arg2,arg3,arg4 ){
+  var arr = [arg1,arg2,arg3,arg4];
+  return arr;
 
-module.exports.firstAndLast = undefined;
+};
+
+module.exports.firstAndLast = function(array){
+
+  var lastElementIndex  = array.length - 1;
+  var returnArray = [array[0],array[lastElementIndex]];
+  return returnArray;
+};
