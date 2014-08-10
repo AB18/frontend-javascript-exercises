3d1ae4667d23091e414eab05df93ef4cb823ecd5
AB <ashrita.b@gmail.com>
2014-08-10 19:05:58 +0000

    Fixed 00-basics

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,44 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration,courseStudents){
+    
+    var course =  {
+        title: courseTitle,
+        duration: courseDuration,
+        students: courseStudents
+    };
+    return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if(!(newProp in object))
+  {
+    object[newProp] = newValue;
+  }
+  return object;
 
-module.exports.formLetter = undefined;
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter){
+  var content = "Hello " + letter.recipient + ",\n\n" + letter.msg 
+   + "\n\nSincerely,\n" + letter.sender;
+   return content; 
+
+};
+
+module.exports.canIGet = function(item, money){
+  var priceChart = [
+  {itemName: 'MacBook Air', itemValue : 999},
+  {itemName: 'MacBook Pro', itemValue : 1299},
+  {itemName: 'Mac Pro', itemValue : 2499},
+  {itemName: 'Apple Sticker', itemValue : 1},
+  ];
+
+  var returnValue = false;
+  for(var i=0;i < priceChart.length ; i++)
+  {
+    if((priceChart[i].itemName == item) && (money >= priceChart[i].itemValue ) )
+    {
+      returnValue = true;
+    }
+  }
+  return returnValue;
+};
