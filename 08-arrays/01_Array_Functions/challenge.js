9c278531886d2b7e87c1e76e6b201dee0f004014
AB <ashrita.b@gmail.com>
2014-08-10 00:31:20 +0000

    Fixed 01_Array_Functions

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,25 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(reverse){
+    reverse.reverse();
+    console.log(reverse);
+    reverse.unshift(1);
+    return reverse;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(combine) {
+    var length = combine.length - 1;
+    var combined = '';
+    for(var i=0; i<= length ; i++)
+    {
+      if(i > 0)
+      {
+        combined += "+";
+      }
+      combined += combine[i];
+    }
+    return combined;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(total){
+    return (total.length + 1);
+
+};
