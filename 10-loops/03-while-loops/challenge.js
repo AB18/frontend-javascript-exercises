7d3040031bd6067ac6e6b44949756673be7a4f57
AB <ashrita.b@gmail.com>
2014-08-12 05:53:46 +0000

    Fixed while loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,19 @@
-module.exports.stream = undefined;
+module.exports.stream = function(condition, action){
+  while(condition() != false) {
+    action();
+  }
 
-module.exports.sumNumbers = undefined;
+};
+
+module.exports.sumNumbers = function(array){
+  var i = 0
+  var sum = 0;
+  while( i < array.length)
+  {
+    sum += array[i];
+    i++;
+  }
+  return sum;
+
+};
 
