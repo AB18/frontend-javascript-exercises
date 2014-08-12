b9be216882743ffc9a937ee23e4f7f0cf11b82f4
AB <ashrita.b@gmail.com>
2014-08-12 04:40:49 +0000

    Fixed looping array

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,29 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arrayPassed){
+  var sum = 0
+  if(arrayPassed.length == 0) {
+    //do nothing
+  }
+  else
+  {
+    for(var i=0; i< arrayPassed.length ; i++)
+    {
+      sum += arrayPassed[i];
+    }
+  }
+  return sum;
 
-module.exports.splitAndLowerCaseString = undefined;
+};
 
-module.exports.addIndex = undefined;
+module.exports.splitAndLowerCaseString = function(inputString){
+  inputString = inputString.toLowerCase();
+  var split = inputString.split(',');
+  return split;
+};
+
+module.exports.addIndex = function(inputValue){
+  var returnArray = [];
+  for(var property in inputValue ){
+      returnArray.push(property + " is " + inputValue[property]);
+  };
+  return returnArray;
+};
