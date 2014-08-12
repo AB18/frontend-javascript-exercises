aa737c4e309d409f27b19aff63ce7a44b3347cf6
AB <ashrita.b@gmail.com>
2014-08-12 04:56:44 +0000

    Fixed iterating on obj

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,26 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(keyValue) {
+  var returnArray = [];
+  for (var property in keyValue) {
+    
+      returnArray.push(property);
+  }
+   return returnArray;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(keyValue) {
+    var returnArray = [];
+    for(var property in keyValue )
+    {
+      returnArray.push(keyValue[property]);
+    }
+    return returnArray;
 
-module.exports.objectToArray = undefined;
+};
+
+module.exports.objectToArray = function(inputValue) {
+  var returnArray = [];
+  for(var property in inputValue ){
+      returnArray.push(property + " is " + inputValue[property]);
+  }
+  return returnArray;
+};
