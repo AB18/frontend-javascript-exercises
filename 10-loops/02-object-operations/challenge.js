114478de1eac0836cd69ab1095a28f098e800865
AB <ashrita.b@gmail.com>
2014-08-12 05:44:04 +0000

    Fixed Object Oerations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,33 @@
-module.exports.copy = undefined;
+module.exports.copy = function(keyValue){
+  var copyObject = {};
+  for(var property in keyValue)
+  {
+    copyObject[property] = keyValue[property];
+  }
+  console.log(copyObject);
+  return copyObject;
 
-module.exports.extend = undefined;
+};
 
-module.exports.hasElems = undefined;
+module.exports.extend = function(dest, src){
+  for(var property in src)
+  {
+    dest[property] = src[property];
+  }
+  console.log(dest);
+  return dest;
+
+};
+
+module.exports.hasElems = function(obj, array) {
+  for(var i = 0; i < array.length; i++) 
+  {
+    if(!(array[i] in obj))
+    {
+      console.log(i);
+      return false;
+    }
+  }
+  return true;
+
+};
